35fa6385535586dfd500a1b5c2fa539ac9f8fa17
Paul Killman <39015713+pkillman2000@users.noreply.github.com>
2025-06-19 14:06:54 -0500

    Section 2 - Vehicle Setup Complete

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -1,53 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
-    public WheelCollider[] WCs;
-    public GameObject[] Wheels;
-    public float torque = 200;
-    public float maxSteerAngle = 30;
-    public float maxBrakeTorque = 500;
+    [SerializeField]
+    private WheelCollider[] _wheelColliders;
+    [SerializeField]
+    private GameObject[] _wheelMeshes;
+    [SerializeField]
+    private float _torque = 200f;
+    [SerializeField]
+    private float _maxBrakeTorque = 500f;
+    [SerializeField]
+    private float _maxSteerAngle = 30.0f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+    }
+    
+    private void Update()
+    {
+        float acceleration = Input.GetAxis("Vertical");
+        float steeringAngle = Input.GetAxis("Horizontal");
+        float brake = Input.GetAxis("Jump"); // Spacebar
+        Go(acceleration, steeringAngle, brake);
     }
 
-    void Go(float accel, float steer, float brake)
+    public void Go(float acceleration, float steeringAngle, float brake)
     {
-        accel = Mathf.Clamp(accel, -1, 1);
-        steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
-        brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
+        Quaternion quat;
+        Vector3 position;
 
-        float thrustTorque = accel * torque;
+        acceleration = Mathf.Clamp(acceleration, -1, 1);
+        steeringAngle = Mathf.Clamp(steeringAngle, -1, 1) * _maxSteerAngle;
+        brake = Mathf.Clamp(brake, 0, 1) * _maxBrakeTorque;
+        float thrustTorque = acceleration * _torque;
 
-        for (int i = 0; i < 4; i++)
+        // Affect each tire
+        for (int i = 0; i < _wheelColliders.Length; i++)
         {
-            WCs[i].motorTorque = thrustTorque;
+            // Only front tires steer
+            if (_wheelColliders[i].gameObject.tag == "Steer Tire")
+            {
+                _wheelColliders[i].steerAngle = steeringAngle;
+            }
 
-            if (i < 2)
-                WCs[i].steerAngle = steer;
-            else
-                WCs[i].brakeTorque = brake;
+            // Brake
+            if (_wheelColliders[i].gameObject.tag == "Brake Tire")
+            {
+                _wheelColliders[i].brakeTorque = brake;
+                Debug.Log("Brake: " + brake);
+            }
 
-            Quaternion quat;
-            Vector3 position;
-            WCs[i].GetWorldPose(out position, out quat);
-            Wheels[i].transform.position = position;
-            Wheels[i].transform.localRotation = quat;
+            // Move
+            if (brake == 0)
+            {
+                _wheelColliders[i].motorTorque = thrustTorque;
+            }
+
+            // Move  and rotate tire mesh - this does NOT cause rotation
+            _wheelColliders[i].GetWorldPose(out position, out quat);
+            _wheelMeshes[i].transform.position = position;
+            _wheelMeshes[i].transform.rotation = quat;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float a = Input.GetAxis("Vertical");
-        float s = Input.GetAxis("Horizontal");
-        float b = Input.GetAxis("Jump");
-
-        Go(a,s,b);
     }
 }
